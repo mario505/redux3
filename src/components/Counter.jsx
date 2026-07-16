6adc74fb5d07aafeacca87c467ebefc900a07320
mario <mario_reyes505@hotmail.com>
2026-07-15 20:44:01 -0500

    added margin on counter buttons. added dispatch funcs on Counter.jsx to handle redux store

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,27 @@
 // import classes from './Counter.module.css';
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
   const toggleCounterHandler = () => {};
 
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
   return (
-    <main className={"counter"}>
+    <main className="counter">
       <h1>Redux Counter</h1>
-      <div className={"value"}>{counter}</div>
+      <div className="value">{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
