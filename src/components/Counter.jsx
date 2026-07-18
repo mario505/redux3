091149fc4373ce449570bce8785366d0b4d96958
mario <mario_reyes505@hotmail.com>
2026-07-17 22:13:19 -0500

    added showCouter state on counterReducer and action to toggle the counter on it, added it to Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,14 +4,17 @@ import { useDispatch, useSelector } from "react-redux";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
-  const toggleCounterHandler = () => {};
+  const show = useSelector((state) => state.showCounter);
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    dispatch({ type: "increase", amount: 10 });
   };
 
   const decrementHandler = () => {
@@ -21,10 +24,10 @@ const Counter = () => {
   return (
     <main className="counter">
       <h1>Redux Counter</h1>
-      <div className="value">{counter}</div>
+      {show && <div className="value">{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
-        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
